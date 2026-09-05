7796741c5ab3843eff69c06595593051c885bccb
umesh <umesh@umeshs-MacBook-Air.local>
2026-09-05 12:49:32 +0530

    chore: remove temporary patch scripts

--- apps/backend/patch_statement.cjs
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('src/workflows/statement-workflow.ts', 'utf8');
-
-content = content.replace(
-  "url: 'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf'",
-  "url: \`http://localhost:3000/api/statements?accountId=\${account.id}&fromDate=\${inputData.fromDate || ''}&toDate=\${inputData.toDate || ''}\`"
-);
-
-fs.writeFileSync('src/workflows/statement-workflow.ts', content);
